966c46a50adad686313c78c5d8569d472302ac39
Matteo Lodi <30625432+mlodic@users.noreply.github.com>
2022-09-20 14:47:57 +0200

    fix for migration to pymongo4

--- djongo/introspection.py
+++ djongo/introspection.py
@@ -38,7 +38,7 @@ class DatabaseIntrospection(BaseDatabaseIntrospection):
     }
 
     # def table_names(self, cursor=None, include_views=False):
-    #     return sorted(cursor.m_cli_connection.collection_names(False))
+    #     return sorted(cursor.m_cli_connection.list_collection_names(False))
 
     def get_table_list(self, cursor):
 
